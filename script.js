8acf22e486d38e121c4f6ff5ed3edf084d7bc4c3
Gilson Aureliano <gilson.aureliano22@gmail.com>
2021-08-12 16:29:42 -0300

    aula12.08

---
+++ script.js
@@ -0,0 +1,28 @@
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var horatxt = window.document.getElementById('hora')
+    var hora = Number(horatxt.value)
+    var msginicial = window.document.getElementById('msginicial')
+    
+    msg.innerHTML = `Agora são ${hora} horas`
+    if (hora > 0 && hora < 12) {
+        //Bom dia
+        img.src = "imagens/manha.png"
+        document.body.style.backgroundColor = "#F4C8AA"
+        msginicial.style.color = 'brown'
+    }else if (hora >= 12 && hora <= 18) {
+        //Boa tarde
+        img.src = "imagens/tarde.png"
+        document.body.style.backgroundColor = "#AA7374"
+        msginicial.style.color = 'black'
+    }else {
+        //Boa noite
+        img.src = "imagens/noite.png"
+        document.body.style.backgroundColor = "#403253"
+        msginicial.style.color = 'white'
+        
+    }
+
+}
+
